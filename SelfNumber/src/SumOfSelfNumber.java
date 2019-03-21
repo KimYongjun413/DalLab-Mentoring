515d67bea74646ab01310202534162956b6b1a80
kimyongjun413 <kimyongjun413@gmail.com>
2019-03-22 01:40:45 +0900

    generators 메서드 및 테스트 코드 작성

--- SelfNumber/src/SumOfSelfNumber.java
+++ SelfNumber/src/SumOfSelfNumber.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
 
 public class SumOfSelfNumber {
     public static void main(String[] args) {
         System.out.printf("공사 중 입니다.");
+    }
+
+    public static int[] generators(int n) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            resultList.add(d(i));
+        }
+        int [] result = convertIntegers(resultList);
+        return result;
     }
 
     public static int d(int n) {
@@ -13,5 +23,12 @@ public class SumOfSelfNumber {
         return result;
     }
 
-
+    public static int[] convertIntegers(ArrayList<Integer> integers)
+    {
+        int[] intNumbers = new int[integers.size()];
+        for (int i=0; i < intNumbers.length; i++) {
+            intNumbers[i] = integers.get(i).intValue();
+        }
+        return intNumbers;
+    }
 }
--- SelfNumber/test/SumOfSelfNumberTest.java
+++ SelfNumber/test/SumOfSelfNumberTest.java
@@ -7,6 +7,13 @@ public class SumOfSelfNumberTest {
     private SumOfSelfNumber naturalNumbers;
 
     @Test
+    public void generators() {
+        assertArrayEquals(new int[]{2}, naturalNumbers.generators(1));
+        assertArrayEquals(new int[]{2, 4, 6, 8}, naturalNumbers.generators(4));
+        assertArrayEquals(new int[]{2, 4, 6, 8, 10}, naturalNumbers.generators(5));
+    }
+
+    @Test
     public void d() {
         assertEquals(2, naturalNumbers.d(1));
         assertEquals(10, naturalNumbers.d(5));
