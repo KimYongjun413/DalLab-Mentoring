7a35ead8fb483c4df96326a18bd39c34c6d80254
kimyongjun413 <kimyongjun413@gmail.com>
2019-04-19 16:10:32 +0900

    작업관리 프로그램 프로토타입

---
+++ To-do-management_CSharp/ToDoMngmtForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace To_do_management_CSharp
+{
+    partial class ToDoMngmtForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.resultGrid = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblTotalTime = new System.Windows.Forms.Label();
+            this.txtTotalTime = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(320, 3);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(100, 21);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "여..여기요...";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(3, 3);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(309, 21);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(431, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 21);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "결과보기";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGrid, 2);
+            this.dataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGrid.Location = new System.Drawing.Point(3, 3);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.RowTemplate.Height = 23;
+            this.dataGrid.Size = new System.Drawing.Size(630, 121);
+            this.dataGrid.TabIndex = 4;
+            // 
+            // resultGrid
+            // 
+            this.resultGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.resultGrid, 2);
+            this.resultGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultGrid.Location = new System.Drawing.Point(3, 130);
+            this.resultGrid.Name = "resultGrid";
+            this.resultGrid.ReadOnly = true;
+            this.resultGrid.RowTemplate.Height = 23;
+            this.resultGrid.Size = new System.Drawing.Size(630, 122);
+            this.resultGrid.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel2.SetColumnSpan(this.tableLayoutPanel1, 2);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.resultGrid, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dataGrid, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 53);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(636, 255);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(642, 361);
+            this.tableLayoutPanel2.TabIndex = 6;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 4;
+            this.tableLayoutPanel2.SetColumnSpan(this.tableLayoutPanel3, 2);
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 317F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 111F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 189F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.txtPath, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnSave, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnUpload, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(636, 44);
+            this.tableLayoutPanel3.TabIndex = 6;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel2.SetColumnSpan(this.tableLayoutPanel4, 2);
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.7673F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 82.2327F));
+            this.tableLayoutPanel4.Controls.Add(this.lblTotalTime, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.txtTotalTime, 1, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Font = new System.Drawing.Font("굴림", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 314);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(636, 44);
+            this.tableLayoutPanel4.TabIndex = 7;
+            // 
+            // lblTotalTime
+            // 
+            this.lblTotalTime.AutoSize = true;
+            this.lblTotalTime.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTotalTime.Location = new System.Drawing.Point(3, 10);
+            this.lblTotalTime.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+            this.lblTotalTime.Name = "lblTotalTime";
+            this.lblTotalTime.Size = new System.Drawing.Size(107, 34);
+            this.lblTotalTime.TabIndex = 0;
+            this.lblTotalTime.Text = "총 운동시간";
+            // 
+            // txtTotalTime
+            // 
+            this.txtTotalTime.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtTotalTime.Font = new System.Drawing.Font("굴림", 20F);
+            this.txtTotalTime.Location = new System.Drawing.Point(116, 3);
+            this.txtTotalTime.Multiline = true;
+            this.txtTotalTime.Name = "txtTotalTime";
+            this.txtTotalTime.ReadOnly = true;
+            this.txtTotalTime.Size = new System.Drawing.Size(517, 38);
+            this.txtTotalTime.TabIndex = 1;
+            // 
+            // ToDoMngmtForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 361);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "ToDoMngmtForm";
+            this.Text = "운동한 정보를 내 놓아라!!";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.DataGridView resultGrid;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Label lblTotalTime;
+        private System.Windows.Forms.TextBox txtTotalTime;
+
+    }
+}
+
---
+++ To-do-management_CSharp/ToDoMngmtForm.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace To_do_management_CSharp
+{
+    public partial class ToDoMngmtForm : Form
+    {
+
+        DataTable exerciseInfoTable = new DataTable();
+        TimeSpan totTime = new TimeSpan();
+
+        public ToDoMngmtForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+
+            string fileName = string.Empty;
+            DataTable rawTable = new DataTable();
+            
+            openFileDialog.Filter = "CSV Files (*.csv)|*.csv";
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileName = openFileDialog.FileName;
+                rawTable = GetDataTableFromCsv(fileName, false);
+            }
+
+            if (fileName.Equals(string.Empty)) return;
+            exerciseInfoTable = rawTable.DefaultView.ToTable(false, new string[] { "exercise_type", "start_time", "end_time" });
+            dataGrid.DataSource = exerciseInfoTable;
+
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string filePath = txtPath.Text;
+
+            resultGrid.DataSource = GetResultTable(exerciseInfoTable);
+
+            if (totTime.TotalMinutes > 50)
+            {
+                MessageBox.Show("유튜브를 해도 좋습니다!");
+            }
+            else
+            {
+                MessageBox.Show("운동도 덜 하고 유튜브를 하려 하다니!! 더 운동하고 오세요!!");
+                File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "127.0.0.1    www.youtube.com", Encoding.UTF8);
+            }
+
+            
+
+        }
+
+        private DataTable GetDataTableFromCsv(string path, bool isFirstRowHeader)
+        {
+            DataTable dt = new DataTable();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                sr.ReadLine();
+                string[] headers = sr.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dt.Columns.Add(header);
+                }
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = sr.ReadLine().Split(',');
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i];
+                    }
+                    dt.Rows.Add(dr);
+                }
+
+            }
+
+            return dt;
+
+        }
+
+        private DataTable GetResultTable(DataTable pExcerciseInfoTable)
+        {
+            DataTable resultTable = new DataTable();
+
+            DataTable tmpTable = new DataTable();
+            tmpTable.Merge(pExcerciseInfoTable, false, MissingSchemaAction.Add);
+
+            tmpTable.Columns.Add("exercise_time");            
+            string strExcsType = string.Empty;            
+
+            for (int iRow = 0; iRow < tmpTable.Rows.Count; iRow++)
+            {
+                strExcsType = tmpTable.Rows[iRow]["exercise_type"].ToString();
+
+                if (strExcsType.Equals("10004") || strExcsType.Equals("10005") ||
+                    strExcsType.Equals("10012") || strExcsType.Equals("10025"))
+                {
+
+                    if (!isToday(tmpTable.Rows[iRow]["end_time"].ToString()))
+                    {
+                        tmpTable.Rows.RemoveAt(iRow);
+                        iRow--;
+                    }
+                    else
+                    {
+                        if (strExcsType.Equals("10004")) tmpTable.Rows[iRow]["exercise_type"] = "Push-ups (Press-ups)";
+                        else if (strExcsType.Equals("10005")) tmpTable.Rows[iRow]["exercise_type"] = "Pull-ups (Chin-up)";
+                        else if (strExcsType.Equals("10012")) tmpTable.Rows[iRow]["exercise_type"] = "Squats";
+                        else if (strExcsType.Equals("10025")) tmpTable.Rows[iRow]["exercise_type"] = "Plank";
+
+
+                        tmpTable.Rows[iRow]["exercise_time"] = Convert.ToDateTime(tmpTable.Rows[iRow]["end_time"]) - Convert.ToDateTime(tmpTable.Rows[iRow]["start_time"]);
+                        totTime = totTime.Add(TimeSpan.Parse(tmpTable.Rows[iRow]["exercise_time"].ToString()));
+                    }
+                }
+                else
+                {
+                    tmpTable.Rows.RemoveAt(iRow);
+                    iRow--;
+                }
+            }
+
+            tmpTable.Rows.Add();
+            tmpTable.Rows[tmpTable.Rows.Count - 1]["exercise_type"] = "총 운동시간";
+            tmpTable.Rows[tmpTable.Rows.Count - 1]["exercise_time"] = totTime.Hours + "시간 " + totTime.Minutes + "분 " + totTime.Seconds + "초";
+
+            resultTable = tmpTable;
+
+            txtTotalTime.Text = totTime.Hours + "시간 " + totTime.Minutes + "분 " + totTime.Seconds + "초";
+
+            return resultTable;
+        }
+
+        private bool isToday(string pDate)
+        {
+            bool result = false;
+
+            //DateTime now = DateTime.Today;
+            DateTime now = new DateTime(2019, 4, 17);
+
+            DateTime date = Convert.ToDateTime(pDate);
+
+            if(now.ToString("d").Equals(date.ToString("d")))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        
+    }
+}
